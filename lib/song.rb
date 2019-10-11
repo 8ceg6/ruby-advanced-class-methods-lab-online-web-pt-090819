58956268c65d08a3aca63d26bf92d7256189dc2c
cedric e goddard <8ceg6@comcast.net>
2019-10-11 21:51:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,5 +31,7 @@ class Song
         self.all.find {|track| track.name == song_name}
     end 
    
-   
+    def self find_or_create_by_name(song_name)
+           self.find_by_name(song_name) || self.create_by_name(song_name)
+         end 
 end
